833b3df3cc225f9a9bb786df04821f14e1e9336b
tombetthauser <tombetthauser@gmail.com>
2019-11-13 00:07:01 -0800

    Solved methods through ice_cream_flavors

--- class_day_files/W5D3_recursion/tom_betthauser_recursion_homework.rb
+++ class_day_files/W5D3_recursion/tom_betthauser_recursion_homework.rb
@@ -48,21 +48,30 @@ end
   end
 
 #   # Test Cases
-  p gamma_fnc(0)  # => returns nil
-  p gamma_fnc(1)  # => returns 1
-  p gamma_fnc(4)  # => returns 6
-  p gamma_fnc(8)  # => returns 5040
+  # p gamma_fnc(0)  # => returns nil
+  # p gamma_fnc(1)  # => returns 1
+  # p gamma_fnc(4)  # => returns 6
+  # p gamma_fnc(8)  # => returns 5040
 
 # Exercise 4 - Ice Cream Shop
 
-# Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream flavors available at the ice cream shop, as well as the user's favorite ice cream flavor. Recursively find out whether or not the shop offers their favorite flavor.
+
+# Write a function ice_cream_shop(flavors, favorite) that takes in an array of 
+# ice cream flavors available at the ice cream shop, as well as the user's 
+# favorite ice cream flavor. Recursively find out whether or not the shop offers their favorite flavor.
+
+def ice_cream_shop(flavors, favorite)
+  return false if flavors.empty?
+  return true if flavors.first == favorite
+  ice_cream_shop(flavors[1..-1], favorite)
+end
 
 #   # Test Cases
-#   ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
-#   ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
-#   ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
-#   ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
-#   ice_cream_shop([], 'honey lavender')  # => returns false
+  p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+  p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+  p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+  p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+  p ice_cream_shop([], 'honey lavender')  # => returns false
 
 # Exercise 5 - Reverse
 
