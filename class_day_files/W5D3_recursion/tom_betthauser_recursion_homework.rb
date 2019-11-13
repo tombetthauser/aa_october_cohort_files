09b6f5e4c50817c8f46d5996af4b6293dd316b6c
tombetthauser <tombetthauser@gmail.com>
2019-11-12 23:57:12 -0800

    Attempted gamma function problem

--- class_day_files/W5D3_recursion/tom_betthauser_recursion_homework.rb
+++ class_day_files/W5D3_recursion/tom_betthauser_recursion_homework.rb
@@ -26,21 +26,32 @@ def add_numbers(nums_array)
   nums_array[0] + add_numbers(nums_array[1..-1])
 end
 
+# ~3min
+
 #   # Test Cases
-  p add_numbers([1,2,3,4]) # => returns 10
-  p add_numbers([3]) # => returns 3
-  p add_numbers([-80,34,7]) # => returns -39
-  p add_numbers([]) # => returns nil
+  # p add_numbers([1,2,3,4]) # => returns 10
+  # p add_numbers([3]) # => returns 3
+  # p add_numbers([-80,34,7]) # => returns -39
+  # p add_numbers([]) # => returns nil
 
 # Exercise 3 - Gamma Function
 
-# Let's write a method that will solve Gamma Function recursively. The Gamma Function is defined Γ(n) = (n-1)!.
+
+
+# Let's write a method that will solve Gamma Function recursively. The Gamma 
+# Function is defined Γ(n) = (n-1)!.
+
+  def gamma_fnc(n)
+    return nil if n == 0
+    return 1 if n == 1
+    gamma_fnc(n-1) * gamma_fnc(n-2)
+  end
 
 #   # Test Cases
-#   gamma_fnc(0)  # => returns nil
-#   gamma_fnc(1)  # => returns 1
-#   gamma_fnc(4)  # => returns 6
-#   gamma_fnc(8)  # => returns 5040
+  p gamma_fnc(0)  # => returns nil
+  p gamma_fnc(1)  # => returns 1
+  p gamma_fnc(4)  # => returns 6
+  p gamma_fnc(8)  # => returns 5040
 
 # Exercise 4 - Ice Cream Shop
 
