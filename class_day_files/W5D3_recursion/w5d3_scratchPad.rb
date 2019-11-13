ce852ae54b0a02c1320b873c62bc1af2bba400b0
tombetthauser <tombetthauser@gmail.com>
2019-11-12 22:13:06 -0800

    Solved quick_sort in under 5min

--- class_day_files/W5D3_recursion/w5d3_scratchPad.rb
+++ class_day_files/W5D3_recursion/w5d3_scratchPad.rb
@@ -1,5 +1,18 @@
+class Array
 
+  def qsort
+    return self if self.length < 2
 
+    pivot = self.first
+    left = self[1..-1].select { |n| n <= pivot }
+    right = self[1..-1].select { |n| n > pivot }
+
+    left.qsort + [pivot] + right.qsort
+  end
+
+end
+
+# solved in under 5min!
 
 
 p a = (1..25).to_a.shuffle
