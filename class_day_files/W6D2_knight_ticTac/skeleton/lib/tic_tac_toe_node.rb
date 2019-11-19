aa7fe31fb331b5076b26abc7da065be3467d6c93
tombetthauser <tombetthauser@gmail.com>
2019-11-19 10:53:41 -0800

    Adds initialize to tictactoenode class

--- class_day_files/W6D2_knight_ticTac/skeleton/lib/tic_tac_toe_node.rb
+++ class_day_files/W6D2_knight_ticTac/skeleton/lib/tic_tac_toe_node.rb
@@ -2,6 +2,9 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
