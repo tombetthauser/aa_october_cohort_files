7185183e3d7597c46d780ea627a04943e611a043
tombetthauser <tombetthauser@gmail.com>
2019-11-19 21:19:57 -0800

    Clears first set of specs for Board class

--- homeworks/W6D3_mancala/tom_betthauser_mancala_skeleton/lib/board.rb
+++ homeworks/W6D3_mancala/tom_betthauser_mancala_skeleton/lib/board.rb
@@ -2,6 +2,10 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @player1 = name1
+    @player2 = name2
+    @cups = Array.new(14) { Array.new(4, :stone) }
+    @cups[0], @cups[6], @cups[13] = [], [], []
   end
 
   def place_stones
@@ -9,13 +13,16 @@ class Board
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos >= 14
+    raise "Starting cup is empty" if start_pos == 0
   end
 
   def make_move(start_pos, current_player_name)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    # helper method to determine whether #make_move returns :switch, :prompt, 
+    # or ending_cup_idx
   end
 
   def render
