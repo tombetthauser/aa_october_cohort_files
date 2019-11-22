e992f9a4e2cfb9c0eb9459d5fe9bec702f58a56e
tombetthauser <tombetthauser@gmail.com>
2019-11-22 07:44:12 -0800

    Starts but does not finish rspec exercise

---
+++ homeworks/W6D5_rspec/tom_betthauser_rspecExercise/spec/dessert_spec.rb
@@ -0,0 +1,51 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  let(:dessert) { Dessert.new("Pastrie", 5, chef) }
+  
+  describe "#initialize" do
+  it "sets a type" do 
+    expect(chef.type).to eq("chef")
+  end
+  
+  it "sets a quantity" do
+    expect(dessert.quantity).to eq(5)
+  end
+  
+  it "starts ingredients as an empty array" do 
+    expect(dessert.ingredients).to eq([])
+  end
+  
+  it "raises an argument error when given a non-integer quantity" do 
+    expect { Dessert.new("Donut", "five", chef) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array"
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array"
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
