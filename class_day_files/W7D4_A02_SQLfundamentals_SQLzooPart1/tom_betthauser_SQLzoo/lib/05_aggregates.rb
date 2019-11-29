13ac593ecbdc354ce22d9be3dbcf112af8cabee7
tombetthauser <tombetthauser@gmail.com>
2019-11-29 13:59:03 -0800

    Solves query 3 in SQLzoo

--- class_day_files/W7D4_A02_SQLfundamentals_SQLzooPart1/tom_betthauser_SQLzoo/lib/05_aggregates.rb
+++ class_day_files/W7D4_A02_SQLfundamentals_SQLzooPart1/tom_betthauser_SQLzoo/lib/05_aggregates.rb
@@ -44,6 +44,12 @@ end
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+  SELECT
+    COUNT(*)
+  FROM
+    countries
+  WHERE
+    area > 1000000
   SQL
 end
 
