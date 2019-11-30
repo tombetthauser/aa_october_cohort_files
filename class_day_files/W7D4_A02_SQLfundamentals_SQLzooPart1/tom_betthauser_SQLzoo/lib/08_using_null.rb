67f6804d98be0f59587249e98cbfa6d746550be7
tombetthauser <tombetthauser@gmail.com>
2019-11-30 12:27:41 -0800

    Completes third query in 08 SQLzoo problem set

--- class_day_files/W7D4_A02_SQLfundamentals_SQLzooPart1/tom_betthauser_SQLzoo/lib/08_using_null.rb
+++ class_day_files/W7D4_A02_SQLfundamentals_SQLzooPart1/tom_betthauser_SQLzoo/lib/08_using_null.rb
@@ -45,6 +45,12 @@ def all_depts_join
   # NB: you can avoid RIGHT OUTER JOIN (and just use LEFT) by swapping
   # the FROM and JOIN tables.
   execute(<<-SQL)
+  SELECT
+    teachers.name, depts.name
+  FROM
+    depts
+  LEFT OUTER JOIN
+    teachers ON depts.id = teachers.dept_id
   SQL
 end
 
