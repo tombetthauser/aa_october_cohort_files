5e49842cb93a367443c625fce05b8dc6a3e2ab30
tombetthauser <tombetthauser@gmail.com>
2019-11-30 12:25:32 -0800

    Completes second query in 08 problem set

--- class_day_files/W7D4_A02_SQLfundamentals_SQLzooPart1/tom_betthauser_SQLzoo/lib/08_using_null.rb
+++ class_day_files/W7D4_A02_SQLfundamentals_SQLzooPart1/tom_betthauser_SQLzoo/lib/08_using_null.rb
@@ -31,6 +31,12 @@ def all_teachers_join
   # Use a type of JOIN that will list all teachers and their department,
   # even if the department in NULL/nil.
   execute(<<-SQL)
+  SELECT
+    teachers.name, depts.name
+  FROM
+    teachers
+  LEFT OUTER JOIN
+    depts ON depts.id = teachers.dept_id
   SQL
 end
 
