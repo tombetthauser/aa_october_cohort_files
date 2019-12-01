172a00a01d00e5f4017ce6b479aad700f678fb93
tombetthauser <tombetthauser@gmail.com>
2019-11-30 16:03:50 -0800

    Completes query 8 in SQLzoo 08 problems

--- class_day_files/W7D4_A02_SQLfundamentals_SQLzooPart1/tom_betthauser_SQLzoo/lib/08_using_null.rb
+++ class_day_files/W7D4_A02_SQLfundamentals_SQLzooPart1/tom_betthauser_SQLzoo/lib/08_using_null.rb
@@ -118,6 +118,15 @@ def teachers_and_divisions
   # Use CASE to show the name of each teacher followed by 'Sci' if
   # the the teacher is in dept 1 or 2 and 'Art' otherwise.
   execute(<<-SQL)
+  SELECT
+    teachers.name,
+    CASE
+      WHEN teachers.dept_id IN (1,2) THEN 'Sci'
+      ELSE 'Art'
+    END
+  FROM
+    teachers
+
   SQL
 end
 
