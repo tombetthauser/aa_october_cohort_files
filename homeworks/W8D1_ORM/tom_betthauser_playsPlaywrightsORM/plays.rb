73e66ee2c134b5a495dd3e7f719bcc18966b9d1c
tombetthauser <tombetthauser@gmail.com>
2019-12-02 08:52:59 -0800

    Works on css exercise for W8D1

--- homeworks/W8D1_ORM/tom_betthauser_playsPlaywrightsORM/plays.rb
+++ homeworks/W8D1_ORM/tom_betthauser_playsPlaywrightsORM/plays.rb
@@ -32,6 +32,26 @@ class Play
     Play.new(request.first)
   end
 
+  def self.find_by_name(name)
+    query = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        id = (
+          SELECT
+            id
+          FROM
+            playwrights
+          WHERE
+            name = ?
+        )
+    SQL
+    return nil if query.empty?
+    query.map { |play| Play.new(play)}
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -61,4 +81,33 @@ class Play
         id = ?
     SQL
   end
+end
+
+
+
+class Playwright
+  attr_accessor :id, :name, :birth_year
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum)  }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database!" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
 end
