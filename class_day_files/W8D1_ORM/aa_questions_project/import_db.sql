3ee5735f0523d3263518139390292c81720417ad
tombetthauser <tombetthauser@gmail.com>
2019-12-02 09:48:46 -0800

    Completes questions CREATE TABLE

--- class_day_files/W8D1_ORM/aa_questions_project/import_db.sql
+++ class_day_files/W8D1_ORM/aa_questions_project/import_db.sql
@@ -1,10 +1,15 @@
 CREATE TABLE users (
   -- column1 datatype
-  id int PRIMARY KEY,
-  fname varcar(255),
-  lname varcar(255)
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
 )
 
 CREATE TABLE questions (
-  
+  id INTEGER PRIMARY KEY,
+  title  TEXT NOT NULL,
+  body TEXT
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id)
 )
