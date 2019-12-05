083d68b342f2d1cb1574ac91c9761a2b4897c692
tombetthauser <tombetthauser@gmail.com>
2019-12-04 17:59:46 -0800

    Cleans out unzipped rails projects

--- classworks/W8D3_activeRecordTwo/tom_betthauser_polls/db/migrate/20191204221208_create_answer_choice.rb
+++
@@ -1,9 +0,0 @@
-class CreateAnswerChoice < ActiveRecord::Migration[5.2]
-  def change
-    create_table :answer_choices do |t|
-      t.string :text, null: false
-      t.integer :question_id, null: false
-      t.timestamps
-    end
-  end
-end
